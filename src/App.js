e7ce5768c1386847be6fcab9ce1debf5afd05b96
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-08-01 17:49:33 +0200

    Finished Todo App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react'
+import TodoList from './TodoList'
+import { v4 as uuid } from 'uuid'
 
-function App() {
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
+export default function App () {
+  const [todos, setTodos] = useState([])
+  const todoTextRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(window.localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    window.localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo (id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo (e) {
+    const name = todoTextRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuid(), text: name, complete: false }]
+    })
+    todoTextRef.current.value = null
+  }
+
+  function handleClearTodos () {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoTextRef} type='text' />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button onClick={handleClearTodos}>Clear Completed Todos</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+    </>
+  )
 }
-
-export default App;
